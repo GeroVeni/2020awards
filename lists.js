9f6398ba1436c535fbf11b72c031f2532c1990a2
Georgios Venizelos <georgiosvenizelos+github@improbable.io>
2020-12-18 02:10:47 +0000

    timer

---
+++ lists.js
@@ -0,0 +1,9 @@
+function makeItem(template, map, parent) {
+  if (parent === undefined) { parent = "DIV"; }
+  let newItem = document.createElement(parent);
+  template.match(/{.*?}/g).forEach(function (value) {
+    template = template.replace(value, map[value.slice(1, -1)]);
+  });
+  newItem.innerHTML = template;
+  return newItem.firstChild;
+}
